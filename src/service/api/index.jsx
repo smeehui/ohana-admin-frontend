5203b1415529057f8d07f795f541ee3bfbe9dc4c
TrongAnh16 <anhtrduong@gmail.com>
2023-04-05 17:10:20 +0700

    update userDetail

--- src/service/api/index.jsx
+++ src/service/api/index.jsx
@@ -1,7 +1,9 @@
 const PORT = 8080;
+// import { USER_DETAILS } from './../../config/routes';
 const MAIN_API = "http://localhost:" + PORT;
 const MIDDLE_PATH = "";
 export const GET_ALL_USER = MAIN_API + MIDDLE_PATH + "/api/users";
+export const USER_DETAILS = MAIN_API + MIDDLE_PATH + "/api/users";
 export const GET_ALL_POST = MAIN_API + MIDDLE_PATH + "/api/posts";
 export const FILTER_USER = MAIN_API + MIDDLE_PATH + "/api/users/filter";
 export const UPDATE_USER_STATUS = MAIN_API + MIDDLE_PATH + "/api/users";
